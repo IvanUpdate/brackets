1ec168b35d2670357e3cdded40e04d3268f9401a
Ivan <mironovvan99@gmail.com>
2021-03-14 18:26:00 +0300

    finished solution

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  brackets = str.split();
-  for (let bracket in brackets) {
-    let status = false;
-    for (let bracketsCon in bracketsConfig){
-      if (bracketsCon[0] == bracket || bracketsCon[1] == bracket){
-        status = true;
-      }
-    }
-    if (status == false) {
+  let brackets = str.split('');
+  let left = [];
+  let right = [];
+  for (item of bracketsConfig) {
+    left.push(item[0]);
+    right.push(item[1]);
+  }
+
+  let stack = [];
+  for (item of brackets) {
+    i = left.indexOf(item);
+    k = right.indexOf(item);
+
+    if(i != -1) {
+      stack.push(i);
+    } else if (stack.length === 0 || stack[stack.length - 1] != k) {
       return false;
+    } else {
+      stack.pop();
+    }
+
+    if(i === k && (stack[stack.length - 1] === i) && (stack[stack.length - 2] === i)) {
+      stack.pop();
+      stack.pop();
     }
   }
-  return true;
+  
+  return (stack.length === 0);
 }
